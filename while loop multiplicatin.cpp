085b41e1b7d76109efc6d460f243931d984fd9c5
priyanshi668 <gurjarpriyanshi114@gmail.com>
2025-05-07 10:48:37 -0700

    Create while loop multiplicatin.cpp

---
+++ while loop multiplicatin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>ile 
+using namespace std;
+
+int main() {
+    int number, i = 1;
+
+    cout << "Enter a number to print its multiplication table: ";
+    cin >> number;
+
+    do {
+        cout << number << " x " << i << " = " << number * i << endl;
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
