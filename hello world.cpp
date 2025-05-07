4fb65e5b92bc447faa51b930108bc46d5b845aed
priyanshi668 <gurjarpriyanshi114@gmail.com>
2025-05-07 12:22:25 -0700

    Create hello world.cpp

---
+++ hello world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print "Hello, World!" to the console
+    cout << "Hello, World!" << endl;
+    return 0;  // Return 0 to indicate successful execution
+}
