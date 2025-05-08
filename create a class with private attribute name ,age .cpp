d15ebcf82711eec78400149c32ec7f6f1ad37cdd
priyanshi668 <gurjarpriyanshi114@gmail.com>
2025-05-08 02:36:00 -0700

    Create create a class with private attribute name ,age .cpp

---
+++ create a class with private attribute name ,age .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+private:
+    string name;
+    int age;
+
+public:
+    void setName(string n) {
+        name = n;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    void setAge(int a) {
+        age = a;
+    }
+
+    int getAge() {
+        return age;
+    }
+};
